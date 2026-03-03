f11c84142ab5bfb7c5fd404019bec0c44b21c9d2
prince <princeas100@gmail.com>
2026-03-03 14:24:58 +0530

    migration files

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar')->nullable();
+            $table->text('bio')->nullable();
+            $table->enum('role',['admin', 'user'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
